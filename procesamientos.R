9348188fc000f8b63911833f5bc19d51e4275af6
Jose Rodriguez de la Fuente <josedelafuente_86@hotmail.com>
2022-08-19 15:51:59 -0300

    Primeros descriptivos

--- procesamientos.R
+++ procesamientos.R
@@ -1,10 +1,13 @@
 # Cargo librerías y bases--------------------
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse, haven)
 
 base1995 <- read_spss("bases/base_1995.sav")
 base2003 <- read_spss("bases/base_2003_2004.sav")
 base2007 <- read_spss("bases/base_2007.sav")
 base2010 <- read_spss("bases/base_2010.sav")
+load("bases/base_2021.RData")
+base2021 <- base_hogar
+rm(base_hogar)
 
 base1960 <- readxl::read_xlsx("bases/germani_sampleA.xlsx", col_names = F)
 
@@ -12,11 +15,21 @@ base1960 <- readxl::read_xlsx("bases/germani_sampleA.xlsx", col_names = F)
 # Transformación de variables -------------
 #1995
 base1995 <- base1995 %>%
-  mutate(nivel_ed = factor(p004a, labels = c("Primaria incompleta", "Primaria completa",
+  mutate(p004a = car::recode(p004a, "99=NA"),
+         nivel_ed = factor(p004a, labels = c("Primaria incompleta", "Primaria completa",
                                              "Secundaria incompleta", "Secundaria completa",
                                              "Terciaria incompleta", "Terciaria completa",
                                              "Universitaria incompleta", "Universitaria completa",
-                                             "Posgrado incompleto", "Posgrado completo")))
+                                             "Posgrado incompleto", "Posgrado completo")),
+         clasesub_1995 = case_when(p089aa == 1 ~ 3,
+                                   p089aa == 2 ~ 2,
+                                   p089aa == 3 ~ 1,
+                                   p089aa > 3 & p089ab == 1 ~ 3,
+                                   p089aa > 3 & p089ab == 2 ~ 2,
+                                   p089aa > 3 & p089ab == 3 ~ 1,
+                                   TRUE ~ NA_real_),
+         clasesub_1995_f = factor(clasesub_1995, labels = c("Clase media-alta", "Clase media",
+                                                            "Clase trabajadora")))
 
 #2003/2004
 base2003 <- base2003 %>%
@@ -76,3 +89,35 @@ base2010 <- base2010 %>%
                                                             "Clase media-baja",
                                                             "Clase media",
                                                             "Clase media-alta")))
+
+#2014-2015
+base2015 <- base2015 %>%
+  mutate(clasesub_2015 = case_when(v260a == 1 ~ 1,
+                                   v260a == 2 ~ 2,
+                                   v260a == 3 ~ 3,
+                                   v260a == 4 ~ 4,
+                                   v260a == 5 ~ 5,
+                                   v260a == 6 ~ 6,
+                                   is.na(v260a) & v261a == 1 ~ 1,
+                                   is.na(v260a) & v261a == 2 ~ 2,
+                                   is.na(v260a) & v261a == 3 ~ 3,
+                                   is.na(v260a) & v261a == 4 ~ 4,
+                                   is.na(v260a) & v261a == 5 ~ 5,
+                                   is.na(v260a) & v261a == 6 ~ 6,
+                                   TRUE ~ NA_real_),
+         clasesub_2015_f = factor(clasesub_2015, labels = c("Clase baja",
+                                                            "Clase obrera",
+                                                            "Clase media-baja",
+                                                            "Clase media",
+                                                            "Clase media-alta",
+                                                            "Clase alta")))
+
+#2021
+base2021 <- base2021 %>%
+  mutate(clasesub_2021 = car::recode(M11.6, "1=6; 2=5; 3=4; 4=3; 5=2; 6=1; 99=NA"),
+         clasesub_2021_f = factor(clasesub_2021, labels = c("Clase baja",
+                                                            "Clase trabajadora",
+                                                            "Clase media-baja",
+                                                            "Clase media",
+                                                            "Clase media-alta",
+                                                            "Clase alta")))
